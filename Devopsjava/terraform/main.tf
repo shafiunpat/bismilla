501c4922addef3034b7d306ad5f53fbe2b216ec7
shafiunp <phatanshafiun@gmail.com>
2023-12-22 19:46:26 +0400

    updated terraformfile

--- Devopsjava/terraform/main.tf
+++ Devopsjava/terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.31.0"
     }
   }
+  backend "s3" {
+    bucket         = "shafiun26022023"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "LockID"
+  }
 }
 
 provider "aws" {
