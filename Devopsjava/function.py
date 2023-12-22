12cdb984bc540d5affa8ed9f9eaefd23ef115fe6
shafiunp <phatanshafiun@gmail.com>
2023-12-22 18:49:19 +0400

    updated terraform file

---
+++ Devopsjava/function.py
@@ -0,0 +1,21 @@
+#a=["shafiun","anisa","asma"]
+#b=["clever","pretty","beautifull"]
+#for i in a:
+ #for j in b:
+   # print(i,j)
+def addition(num1,b):
+    add = a+b
+    return(add)
+def addition(a,b):
+    sub = a-b
+    return(s)
+def addition(a,b):
+    mul = a*b
+    return(m)
+print(addition(20,30))
+print(sub(20,30))
+print(mul(20,30))
+
+
+
+    
