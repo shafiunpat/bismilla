80c0e3d3864339b37e8901b092c7a64f2487df19
shafiunp <phatanshafiun@gmail.com>
2023-12-22 23:31:11 +0400

    updated terraform file

--- Devopsjava/terraform/main.tf
+++ Devopsjava/terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket         = "shafiun26022023"
     key            = "terraform.tfstate"
-    region         =  "us-east-1"
+    region         =  "us-east-1s"
     #dynamodb_table = "LockID"
   }
 }
