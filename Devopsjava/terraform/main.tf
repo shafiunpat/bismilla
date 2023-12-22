54940ae719732915b8b6868de59e630ed3cd43ed
shafiunp <phatanshafiun@gmail.com>
2023-12-22 22:18:10 +0400

    updated terrraform file

--- Devopsjava/terraform/main.tf
+++ Devopsjava/terraform/main.tf
@@ -36,7 +36,7 @@ Name  = "internet_gateway"
 resource "aws_subnet" "my_public_subnet" {
 vpc_id                  = aws_vpc.my_vpc.id
 cidr_block              = "10.0.0.0/24"
-availability_zone       = "ap-south-1b"
+availability_zone       = "us-east-1"
 map_public_ip_on_launch = true
 tags      = {
 Name    = "public-subnet"
