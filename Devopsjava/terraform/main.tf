140a8c61e898b8f04de3d011c851726b00baee69
shafiunp <phatanshafiun@gmail.com>
2023-12-30 21:55:55 +0400

    updated terraform file

--- Devopsjava/terraform/main.tf
+++ Devopsjava/terraform/main.tf
@@ -23,6 +23,7 @@ cidr_block = "10.0.0.0/16"
 enable_dns_hostnames    = true
 tags      = {
 Name    = "my_VPC"
+vpc_id   =var.vpc_id
 }
 }
 # Create Internet Gateway
@@ -40,6 +41,7 @@ availability_zone       = "us-east-1a"
 map_public_ip_on_launch = true
 tags      = {
 Name    = "public-subnet"
+subnet_id           =var.subnet_id
 }
 }
 # Create Route Table
@@ -56,7 +58,7 @@ Name     = "Public Route Table"
 # Associate Public Subnet
 resource "aws_route_table_association" "my_public_subnet_route_table_association" {
 subnet_id           = aws_subnet.my_public_subnet.id
-route_table_id      = aws_route_table.my_public_route_table.id
+route_table_id      = aws_route_table.my_public_route_table.id 
 }
 # Create Security Group 
 resource "aws_security_group" "my_security_group" {
