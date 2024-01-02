143d4b6d385f6dd4d61fe54cb1a7d318d894d503
shafiunp <phatanshafiun@gmail.com>
2024-01-02 12:05:39 +0400

    updated terraform file

--- Devopsjava/terraform/main.tf
+++ Devopsjava/terraform/main.tf
@@ -19,55 +19,53 @@ provider "aws" {
   region = var.region
 }
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.10.0.0/16"  # Replace with your desired CIDR block
-  #cidr_block              = var.vpc_value
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags      = {
-Name    = "my_vpc"
+#cidr_block =   "10.0.0.0/16"  
+cidr_block              = var.vpc_value
+enable_dns_hostnames    = true
+tags      = {
+Name    = "my_VPC"
 #vpc_id   =var.vpc_id
 }
 }
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  #cidr_block              = "10.0.1.0/24"  # Replace with your desired subnet CIDR block
-  cidr_block              = var.subnet_value
-  availability_zone       = "us-east-1a"   # Replace with your desired availability zone
-  map_public_ip_on_launch = true
-  tags      = {
+# Create Internet Gateway
+resource "aws_internet_gateway" "my_internet_gateway" {
+vpc_id    = aws_vpc.my_vpc.id
+tags = {
+Name  = "internet_gateway"
+}
+}
+# Create Public Subnet
+resource "aws_subnet" "my_public_subnet" {
+vpc_id                  = aws_vpc.my_vpc.id
+#cidr_block              = "10.0.0.0/24"
+cidr_block              = var.subnet_value
+availability_zone       = "us-east-1a"
+map_public_ip_on_launch = true
+tags      = {
 Name    = "public-subnet"
 #subnet_id           =var.subnet_id
 }
 }
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-Name  = "internet_gateway"
+# Create Route Table
+resource "aws_route_table" "my_public_route_table" {
+vpc_id       = aws_vpc.my_vpc.id
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.my_internet_gateway.id
 }
-}
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
-  tags       = {
+tags       = {
 Name     = "Public Route Table"
 }
 }
-
-resource "aws_route_table_association" "my_subnet_association" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
+# Associate Public Subnet
+resource "aws_route_table_association" "my_public_subnet_route_table_association" {
+subnet_id           = aws_subnet.my_public_subnet.id
+route_table_id      = aws_route_table.my_public_route_table.id 
 }
 # Create Security Group 
 resource "aws_security_group" "my_security_group" {
 name        = "SSH Security Group"
-description = "allow access on Ports 22"
+description = "Enable SSH access on Port 22"
 vpc_id      = aws_vpc.my_vpc.id
 ingress {
 description      = "SSH Access"
@@ -76,14 +74,6 @@ to_port          = 22
 protocol         = "tcp"
 cidr_blocks      = ["0.0.0.0/0"]
 }
-
-# ingress {
-# description      = "Custom TCP Access"
-# from_port        = 8080
-# to_port          = 8080
-# protocol         = "tcp"
-# cidr_blocks      = ["0.0.0.0/0"]
-# }
 egress {
 from_port        = 0
 to_port          = 0
@@ -94,14 +84,13 @@ tags   = {
 Name = "SSH Security Group"
 }
 }
-
 #Create a new EC2 launch configuration
 resource "aws_instance" "my_instance" {
 ami                    = "ami-079db87dc4c10ac91"
 instance_type               = var.instance_type
 key_name                    = var.key_name
 security_groups             = [aws_security_group.my_security_group.id]
-subnet_id                   = aws_subnet.my_subnet.id
+subnet_id                   = aws_subnet.my_public_subnet.id
 associate_public_ip_address = true
 lifecycle {
 create_before_destroy = true
@@ -110,3 +99,5 @@ tags = {
 "Name" = "my_instance"
 }
 }
+
+
