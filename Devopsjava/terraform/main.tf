ac68f59f17ad8513dcde140f6c936b7378db7f4e
shafiunp <phatanshafiun@gmail.com>
2024-01-02 11:21:26 +0400

    updated terraform file

--- Devopsjava/terraform/main.tf
+++ Devopsjava/terraform/main.tf
@@ -18,50 +18,48 @@ provider "aws" {
   # Configuration options
   region = var.region
 }
-# Create VPC
 resource "aws_vpc" "my_vpc" {
-#cidr_block = "10.0.0.0/16"
-cidr_block              = var.vpc_value
-enable_dns_hostnames    = true
-tags      = {
-Name    = "my_VPC"
+  #cidr_block = "10.0.0.0/16"  # Replace with your desired CIDR block
+  cidr_block              = var.vpc_value
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags      = {
+Name    = "my_vpc"
 #vpc_id   =var.vpc_id
 }
 }
-# Create Internet Gateway
-resource "aws_internet_gateway" "my_internet_gateway" {
-vpc_id    = aws_vpc.my_vpc.id
-tags = {
-Name  = "internet_gateway"
-}
-}
-# Create Public Subnet
-resource "aws_subnet" "my_public_subnet" {
-vpc_id                  = aws_vpc.my_vpc.id
-#cidr_block              = "10.0.0.0/24"
-cidr_block              = var.subnet_value
-availability_zone       = "us-east-1a"
-map_public_ip_on_launch = true
-tags      = {
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  #cidr_block              = "10.0.1.0/24"  # Replace with your desired subnet CIDR block
+  cidr_block              = var.subnet_value
+  availability_zone       = "us-east-1a"   # Replace with your desired availability zone
+  map_public_ip_on_launch = true
+  tags      = {
 Name    = "public-subnet"
 #subnet_id           =var.subnet_id
 }
 }
-# Create Route Table
-resource "aws_route_table" "my_public_route_table" {
-vpc_id       = aws_vpc.my_vpc.id
-route {
-cidr_block = "0.0.0.0/0"
-gateway_id = aws_internet_gateway.my_internet_gateway.id
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+Name  = "internet_gateway"
 }
-tags       = {
+}
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+  tags       = {
 Name     = "Public Route Table"
 }
 }
-# Associate Public Subnet
-resource "aws_route_table_association" "my_public_subnet_route_table_association" {
-subnet_id           = aws_subnet.my_public_subnet.id
-route_table_id      = aws_route_table.my_public_route_table.id 
+
+resource "aws_route_table_association" "my_subnet_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
 }
 # Create Security Group 
 resource "aws_security_group" "my_security_group" {
@@ -92,6 +90,7 @@ tags   = {
 Name = "SSH Security Group"
 }
 }
+
 #Create a new EC2 launch configuration
 resource "aws_instance" "my_instance" {
 ami                    = "ami-079db87dc4c10ac91"
